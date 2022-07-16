af036f4de32edcdb61b06fb55b0235408caf1f16
«oleglust» <filslip@yandex.ru>
2022-07-16 16:26:25 +0300

    username

---
+++ USERNAMES/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("enter name:");
+String username = Console.ReadLine();
+
+if(username == "GAY")
+{
+      Console.WriteLine("Fisting is 300$");
+}
+else
+{
+    Console.Write("Oh, Hi ");
+    Console.WriteLine(username);
+}
